c3733c932d821ebeb9c7f522b6c30eb31316c169
Toby Batch <tobias@neontribe.co.uk>
2025-04-11 15:41:34 +0000

    feat: added tailwind

---
+++ README.md
@@ -0,0 +1,1 @@
+https://medium.com/@xinranma/i-tested-4-ai-tools-to-generate-ui-from-the-same-prompt-0d2113736cce
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+        // Add other template paths if needed
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
