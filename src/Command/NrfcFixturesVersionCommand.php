a7780610cd37abea949f8c43809a9a69f4e33b03
Toby Batch <tobias@neontribe.co.uk>
2025-05-02 10:51:38 +0100

    feat: added version support

---
+++ src/Command/NrfcFixturesVersionCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(name: 'nrfc:fixtures:version')]
+final class NrfcFixturesVersionCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Receive version information')
+            ->setHelp('This command allows you to fetch various version information about the app.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $version = '0.0.1';
+
+        $io->writeln($version);
+
+        return Command::SUCCESS;
+    }
+}
+
