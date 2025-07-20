2b87b70977c711fbfc4cfa5506046e9d2c146f0c
Toby Batch <tobias@neontribe.co.uk>
2025-07-20 20:45:07 +0100

    feat: add display name to fixture

--- src/Form/FixtureType.php
+++ src/Form/FixtureType.php
@@ -12,6 +12,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -23,6 +24,10 @@ class FixtureType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('name', TextType::class, [
+                'label' => 'Display Name',
+                'required' => false,
+            ])
             ->add('date', DateType::class, [
                 'widget' => 'single_text',
                 'html5' => false, // allows custom format
