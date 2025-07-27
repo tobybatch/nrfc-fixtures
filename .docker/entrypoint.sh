2911e612749165273b2ce573b98edd86bfb1327b
Toby Batch <tobias@neontribe.co.uk>
2025-07-27 12:17:50 +0100

    fix: prod startup fix

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,104 +2,32 @@
 
 NRFCFIXTURES=$(cat /opt/nrfcfixtures/version.txt)
 
-function waitForDB() {
-  echo "Wait for database connection ..."
-  until php /dbtest.php "$DATABASE_URL"; do
-    echo Checking DB: $?
-    sleep 3
-  done
-  echo "Connection established"
-}
-
-function setMemoryLimit() {
-  # set mem limits and copy in custom logger config
-  if [ -z "$memory_limit" ]; then
-    memory_limit=512M
-  fi
-  sed -i "s/memory_limit.*/memory_limit=$memory_limit/g" /usr/local/etc/php/php.ini
-}
-
-function createUserAndGroup() {
-  if [ -z "$USER_ID" ]; then
-    USER_ID=$(id -u www-data)
-  fi
-  if [ -z "$GROUP_ID" ]; then
-    GROUP_ID=$(id -g www-data)
-  fi
-
-  # Create group if needed
-  if grep -w "$GROUP_ID" /etc/group &>/dev/null; then
-    echo "Group already exists"
-  else
-    echo "www-nrfcfixtures:x:$GROUP_ID:" >> /etc/group
-    grpconv
-  fi
-
-  # Create user if needed
-  if getent passwd "$USER_ID" &>/dev/null; then
-    echo "User already exists"
-  else
-    echo "www-nrfcfixtures:x:$USER_ID:$GROUP_ID:www-nrfcfixtures:/var/www:/usr/sbin/nologin" >> /etc/passwd
-    pwconv
-  fi
-
-  # Return the username for the UID
-  getent passwd "$USER_ID" | cut -d: -f1
-}
-
-function configureWebserver() {
-  if [ -e /use_apache ]; then
-    export APACHE_RUN_USER=$(id -nu "$USER_ID")
-    # This doesn't _exactly_ run as the specified GID, it runs as the GID of the specified user but WTF
-    export APACHE_RUN_GROUP=$(id -ng "$USER_ID")
-    export APACHE_PID_FILE=/var/run/apache2/apache2.pid
-    export APACHE_RUN_DIR=/var/run/apache2
-    export APACHE_LOCK_DIR=/var/lock/apache2
-    export APACHE_LOG_DIR=/var/log/apache2
-    export LANG=C
-  elif [ -e /use_fpm ]; then
-    sed -i "s/user = .*/user = $USER_ID/g" /usr/local/etc/php-fpm.d/www.conf
-    sed -i "s/group = .*/group = $GROUP_ID/g" /usr/local/etc/php-fpm.d/www.conf
-    echo "Setting fpm to run as ${USER_ID}:${GROUP_ID}"
-  else
-    echo "Error, unknown server type"
-  fi
-}
-
-function tweakSymfony() {
-  cp /assets/monolog.yaml /opt/nrfcfixtures/config/packages/monolog.yaml
-  touch .env
-  /opt/nrfcfixtures/bin/console cache:clear
-  composer install
-  /opt/nrfcfixtures/bin/console doctrine:migrations:migrate --no-interaction
-
-  if [ "$APP_ENV" == "dev" ]; then
-      bin/console doctrine:schema:create -n
-      bin/console doctrine:fixtures:load -n
-  fi
-}
-
-function runServer() {
-  echo "$NRFCFIXTURES" > /opt/nrfcfixtures/var/installed
-  echo "NRFC Fixtures is ready"
-  if [ -e /use_apache ]; then
-    exec /usr/sbin/apache2 -D FOREGROUND
-  elif [ -e /use_fpm ]; then
-    exec php-fpm
-  else
-    echo "Error, unknown server type"
-  fi
-}
-
 touch .in_startup
-# needs root
-setMemoryLimit
-createUserAndGroup
-configureWebserver
+echo "Wait for database connection ..."
+until php /dbtest.php "$DATABASE_URL"; do
+  echo Checking DB: $?
+  sleep 3
+done
+echo "Connection established"
 
-# run as user
-DELEGATED_USER=$(createUserAndGroup)
-su - "$USERNAME" -c waitForDB
-su - "$USERNAME" -c tweakSymfony
+cp /assets/monolog.yaml /opt/nrfcfixtures/config/packages/monolog.yaml
+touch .env
+/opt/nrfcfixtures/bin/console cache:clear
+composer install
+/opt/nrfcfixtures/bin/console doctrine:migrations:migrate --no-interaction
+
+if [ "$APP_ENV" == "dev" ]; then
+    bin/console doctrine:schema:create -n
+    bin/console doctrine:fixtures:load -n
+fi
+
+echo "$NRFCFIXTURES" > /opt/nrfcfixtures/var/installed
+echo "NRFC Fixtures is ready"
 rm -f .in_startup
-su - "$USERNAME" -c runServer
+if [ -e /use_apache ]; then
+  exec /usr/sbin/apache2 -D FOREGROUND
+elif [ -e /use_fpm ]; then
+  exec php-fpm
+else
+  echo "Error, unknown server type"
+fi
--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -1,48 +1,49 @@
-#!/bin/bash
+#!/bin/bash -x
 
 OPDIR="$(dirname "$0")"
 OS_TYPE="$(uname -s)"
 
 echo "==> Installing dependencies..."
 
-case "$OS_TYPE" in
-    Darwin)
-        echo "Running on macOS"
-        "$OPDIR/_install-dependencies-mac.sh"
-        ;;
-    Linux)
-        echo "Running on Linux"
-        "$OPDIR/_install-dependencies-linux.sh"
-        ;;
-#    CYGWIN*|MINGW32*|MSYS*|MINGW*)
-#        echo "Running on Windows"
-#        "$OPDIR/install-dependencies-win.sh"
+#case "$OS_TYPE" in
+#    Darwin)
+#        echo "Running on macOS"
+#        "$OPDIR/_install-dependencies-mac.sh"
 #        ;;
-    *)
-        echo "Unknown OS: $OS_TYPE"
-        "$OPDIR/_install-dependencies-unsupported.sh"
-        ;;
-esac
+#    Linux)
+#        echo "Running on Linux"
+#        "$OPDIR/_install-dependencies-linux.sh"
+#        ;;
+##    CYGWIN*|MINGW32*|MSYS*|MINGW*)
+##        echo "Running on Windows"
+##        "$OPDIR/install-dependencies-win.sh"
+##        ;;
+#    *)
+#        echo "Unknown OS: $OS_TYPE"
+#        "$OPDIR/_install-dependencies-unsupported.sh"
+#        ;;
+#esac
 
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 DOCKER_DIR=$(realpath "$OPDIR"/../.docker)
 DOCKER_ENV="$DOCKER_DIR/dev.env"
+COMPOSE_ENV="$DOCKER_DIR/.env"
 
 if [ ! -e "$DOCKER_ENV" ]; then
   cp "$DOCKER_DIR/sample.dev.env" "$DOCKER_ENV"
 fi
 
-USER_ID=$(id -u)
-GROUP_ID=$(id -g)
+UID=$(id -u)
+GID=$(id -g)
 
-if ! grep -q "\bUSER_ID\b" "$DOCKER_ENV"; then
-    echo "USER_ID=$USER_ID" >> "$DOCKER_ENV"
+if [ ! -e "$COMPOSE_ENV" ]; then
+  echo "UID=$UID" > "$COMPOSE_ENV"
+  echo "GID=$GID" >> "$COMPOSE_ENV"
 fi
 
-if ! grep -q "\bGROUP_ID\b" "$DOCKER_ENV"; then
-    echo "GROUP_ID=$GROUP_ID" >> "$DOCKER_ENV"
-fi
+sed -i 's/^UID=$/UID=$(id -u)/' "$COMPOSE_ENV"
+sed -i 's/^GID=$/GID=$(id -g)/' "$COMPOSE_ENV"
 
 touch .env
 echo "Dependencies installed"
