a15165b73ad13ee788bc43fb3df6302eccfc2001
Toby Batch <tobias@neontribe.co.uk>
2025-08-11 21:00:31 +0100

    fix: encore container startup

--- bin/watch.sh
+++ bin/watch.sh
@@ -1,18 +1,26 @@
 #!/bin/bash -x
 
+mkdir -p /opt/nrfcfixtures/.home
+chown -R "${UID}:${GID}" /opt/nrfcfixtures/.home
+chmod -R u+rwX /opt/nrfcfixtures/.home
+export HOME=/opt/nrfcfixtures/.home
+
+# Set Yarn cache and global folders
 mkdir -p /opt/nrfcfixtures/.yarn/cache
 mkdir -p /opt/nrfcfixtures/.yarn/global
 chown -R "${UID}:${GID}" /opt/nrfcfixtures/.yarn
 chmod -R u+rwX /opt/nrfcfixtures/.yarn
-yarn config set cache-folder /opt/nrfcfixtures/.yarn/cache
-yarn config set global-folder /opt/nrfcfixtures/.yarn/global
 
-ls -la
+# Configure Yarn to use the custom directories
+yarn config set cache-folder /opt/nrfcfixtures/.yarn/cache --home-dir /opt/nrfcfixtures/.home
+yarn config set global-folder /opt/nrfcfixtures/.yarn/global --home-dir /opt/nrfcfixtures/.home
+yarn config set rc-path /opt/nrfcfixtures/.home/.yarnrc --home-dir /opt/nrfcfixtures/.home
+
 while [ -e .in_startup ]; do
   echo 'Encore waiting for start up to complete...'
   sleep 10
 done
 echo ">> Running yarn"
-exec yarn install --ignore-prepare
+yarn install #--ignore-prepare
 echo ">> Watching..."
 exec yarn watch
