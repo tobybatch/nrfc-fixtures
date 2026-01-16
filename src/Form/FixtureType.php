627bb2f569bdc600170ac426042bc390a155ae37
Toby Batch <tobias@neontribe.co.uk>
2026-01-16 12:56:58 +0000

    fix: split out the time/date into two fields

--- src/Form/FixtureType.php
+++ src/Form/FixtureType.php
@@ -20,6 +20,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
